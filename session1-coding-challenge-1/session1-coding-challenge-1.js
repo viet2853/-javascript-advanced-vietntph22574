ed24a7a6488d5af6e65077fcec5bd9a493e8cd8b
VieTGioIHOa <vietntph22574@fpt.edu.vn>
2023-01-07 10:23:49 +0700

    session1-coding-challenge-1

---
+++ session1-coding-challenge-1/session1-coding-challenge-1.js
@@ -0,0 +1,10 @@
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+
+console.log(BMIMark, BMIJohn, markHigherBMI);
